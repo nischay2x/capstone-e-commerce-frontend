f34894d627790e5ecf38039437144874293281d1
Nischay Chandra <chandranischay@gmail.com>
2024-06-04 10:49:29 +0530

    fix: public env variables

--- app/constants.ts
+++ app/constants.ts
@@ -1,3 +1,4 @@
 export const NO_BAR_ROUTES = ["/auth/login", "/auth/register", "/auth/register/seller", "/auth/logout", "/", "/auth/forget-password"];
-export const API_BASE = process.env.API_BASE || "http://localhost:5000/api";
+export const PUBLIC_API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+export const API_BASE = process.env.API_BASE;
 export const OPEN_ROUTES = ["/shop", "/"]
--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { API_BASE } from "@/app/constants";
-import { toast } from "@/components/ui/use-toast";
+import { API_BASE, PUBLIC_API_BASE } from "@/app/constants";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
 
 const fetcher = axios.create({
-    baseURL: API_BASE
+    baseURL: PUBLIC_API_BASE
 });
 
 export async function authFetcher() {
